33aed35ba5183069fc118323f86d4e336761d38d
JayaSree <js@eventdesk.co>
2024-01-06 22:56:56 -0500

    added category filter

--- src/components/products-list.jsx
+++ src/components/products-list.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import Product from './product';
 import styles from './products-list.module.css';
 
 function ProductsList() {
   const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
 
   useEffect(() => {
     const url = 'https://dummyjson.com/products';
@@ -11,7 +12,10 @@ function ProductsList() {
       try {
         const response = await fetch(url);
         const result = await response.json();
+        // console.log(result.products);
         setProducts(result.products);
+        // let categories = [...new Set(result.products.map((p) => p.category))];
+        // console.log(categories);
       } catch (error) {
         console.log('error', error);
       }
@@ -19,19 +23,50 @@ function ProductsList() {
     fetchData();
   }, []);
 
+  function getFilteredList() {
+    if (!selectedCategory) {
+      return products;
+    }
+    return products.filter((item) => item.category === selectedCategory);
+  }
+
+  let filteredList = useMemo(getFilteredList, [selectedCategory, products]);
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
   return (
-    <div className={styles.cardsList}>
-      {products.map((p) => (
-        <div key={p.id}>
-          <Product
-            id={p.id}
-            img={p.thumbnail}
-            title={p.title}
-            desc={p.description}
-            price={`$${p.price}`}
-          />
-        </div>
-      ))}
+    <div>
+      <div>
+        <select
+          name="category-list"
+          id="category-list"
+          onChange={handleCategoryChange}
+          className={styles.categoryDropdown}
+        >
+          <option value="">All</option>
+          <option value="smartphones">Smartphones</option>
+          <option value="laptops">Laptops</option>
+          <option value="fragrances">Fragrances</option>
+          <option value="skincare">Skincare</option>
+          <option value="groceries">Groceries</option>
+          <option value="home-decoration">Home-decoration</option>
+        </select>
+      </div>
+      <div className={styles.cardsList}>
+        {filteredList.map((p) => (
+          <div key={p.id}>
+            <Product
+              id={p.id}
+              img={p.thumbnail}
+              title={p.title}
+              desc={p.description}
+              price={`$${p.price}`}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
